a15901e2b980e41f1bb0a7565f00556d434d5b9d
Anuj Vijay  Loharkar <127491755+Anuj3558@users.noreply.github.com>
2024-11-20 12:18:15 +0530

    Update server.js

--- invoice-backend/server.js
+++ invoice-backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "*" })); // Allow all origins
 app.use(express.json());
 app.use(express.static("uploads")); // Serve uploaded files
 
